c5b6fbbaacfcdcf92cf0efa98312c5d6924f362b
Janis Vahsteins <janisvahsteins@gmail.com>
2019-06-05 12:25:56 +0300

    media queries for responsive layout

--- vanillascript.js
+++ vanillascript.js
@@ -3,6 +3,10 @@ let startingvalue = parseInt(document.getElementById("startval").value, 1);
 let endingvalue =  parseInt(document.getElementById("endval").value, 1);
 let fizzvalue = parseInt(document.getElementById("fizzval").value, 1);
 let buzzvalue = parseInt(document.getElementById("buzzval").value, 1);
+let output1 = document.getElementById('output1');
+let output2 = document.getElementById('output2');
+let output3 = document.getElementById('output3');
+let output4 = document.getElementById('output4');
 
 //Listeners
 document.getElementById('makedivs').addEventListener("click", generateDivs);
@@ -10,41 +14,45 @@ document.getElementById('cleardivs').addEventListener("click", deleteDivs);
 
 //Logic
 function createDivs(startingvalue, endingvalue, fizzvalue, buzzvalue) {
+    if (document.getElementById('divgen').childElementCount > 0) {
+        return alert("Please delete existing divs first!")
+    }
     const cont = document.getElementById('divgen');
     for (let i = startingvalue; i <= endingvalue; i++) {
         let creatediv = document.createElement('div');
         creatediv.setAttribute('id', 'div' + i);
-        creatediv.textContent = "" + i;
-
         if (i % fizzvalue === 0 && i % buzzvalue === 0) {
             creatediv.setAttribute("class", 'fizzbuzz');
+            creatediv.innerText = "FizzBuzz";
         } else if (i % fizzvalue === 0) {
             creatediv.setAttribute("class", 'fizz');
+            creatediv.innerText = "Fizz";
         } else if (i % buzzvalue === 0) {
             creatediv.setAttribute("class", 'buzz');
+            creatediv.innerText = "Buzz";
         } else {
             creatediv.setAttribute("class", 'normal');
+            creatediv.textContent = "" + i
         }
         cont.appendChild(creatediv);
     }
-    console.log("createDivs executed");
 }
 
 //Button Functions
 function deleteDivs() {
   for(i = document.getElementById('divgen').childElementCount; i > 0; i--) {
       document.getElementById('div' + i).remove();
-      console.log("Deleted element with id " + i)
+output1.value = 0;
+output2.value = 0;
+output3.value = 0;
+output4.value = 0;
   }
-    console.log(document.getElementById('divgen').childElementCount);
 }
 
 function generateDivs() {
-    console.log("Divs created");
-createDivs(1, 30, 3, 5);
+createDivs(1, 60, 3, 5);
+output1.value = document.getElementsByClassName('fizzbuzz').length;
+output2.value = document.getElementsByClassName('fizz').length;
+output3.value = document.getElementsByClassName('buzz').length;
+output4.value = document.getElementsByClassName('normal').length;
 }
-
-console.log('Running');
-
-
-
